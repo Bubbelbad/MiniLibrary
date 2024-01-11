82b8c55d9f4789fd07a0659001d055c74a5ff339
Bubbelbad <victor.ivarson@hotmail.se>
2024-01-11 13:23:33 +0100

    Add project files.

---
+++ MiniLibrary/Classes/BorrowPeriod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniLibrary.Classes
+{
+    internal class BorrowPeriod
+    {
+        public BorrowPeriod() { }
+
+        public int id { get; set; }
+        public DateTime startTime {  get; set; }
+        public DateTime endTime {  get; set; }
+        public bool isReturned { get; set; }
+        public int bookId { get; set; }
+        public int customerId {  get; set; }
+    }
+}
---
+++ MiniLibrary/Classes/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniLibrary.Classes
+{
+    internal class Notification
+    {
+        public int id { get; set; }
+        public DateTime created { get; set; }
+        public string type { get; set; }
+        public int customerId { get; set; }
+    }
+}
