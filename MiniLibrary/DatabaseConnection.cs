cc999564c9964f8d29b8f905662d29f96fb296fb
Bubbelbad <victor.ivarson@hotmail.se>
2024-01-12 17:59:00 +0100

    DatabaseConnection class

---
+++ MiniLibrary/DatabaseConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniLibrary
+{
+    class DatabaseConnection
+    {
+        string server = "localhost";
+        string database = "MiniLibrary"; 
+        string username = "root"; 
+        string password = "Abc123!";
+
+        string connectionString;
+
+        public DatabaseConnection()
+        {
+            connectionString =
+                "SERVER=" + server + ";" +
+                "DATABASE=" + database + ";" +
+                "UID=" + username + ";" +
+                "PASSWORD=" + password + ";";
+            Console.WriteLine("ConnectionString: " + connectionString);
+        }
+    }
+}
